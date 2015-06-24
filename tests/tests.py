03f23f72e3c29ba4c083c38f5d21f0c73054f94a
cmmorrow <cmmorrow@gmail.com>
2015-06-23 22:51:32 -0500

    Added Linear Regression

--- tests/tests.py
+++ tests/tests.py
@@ -13,7 +13,7 @@ class Test:
         self.data = data
         self.alpha = alpha
         self.display = display
-        self.results = 0, 1
+        self.results = 1, 0
 
         """If data is not a vector, wrap it in a vector object """
         if not operations.is_vector(data):
@@ -23,10 +23,13 @@ class Test:
         self.data = operations.drop_nan(self.data)
 
         """Run the test and display the results"""
+        self.logic()
+
+    def logic(self):
         self.results = self.run()
-        if display:
+        if self.display:
             self.output()
-            if self.results[1] > alpha:
+            if self.results[0] > self.alpha:
                 self.h0()
             else:
                 self.ha()
@@ -36,7 +39,7 @@ class Test:
         return 0, 1
 
     def output(self):
-        print str(self.results[0]) + ", " + str(self.results[1])
+        print str(self.results[1]) + ", " + str(self.results[0])
 
     def h0(self):
         print "H0: "
@@ -45,13 +48,38 @@ class Test:
         print "HA: "
 
 
+class Comparison(Test):
+
+    __min_size = 2
+
+    def __init__(self, xdata, ydata, alpha=0.05, display=True):
+
+        self.xdata = xdata
+        self.ydata = ydata
+        self.alpha = alpha
+        self.display = display
+        self.results = 1, 0
+
+        if not operations.is_vector(xdata):
+            self.xdata = vector.Vector(xdata)
+        if not operations.is_vector(ydata):
+            self.ydata = vector.Vector(ydata)
+
+        self.xdata, self.ydata = operations.drop_nan_intersect(self.xdata, self.ydata)
+
+        if len(self.xdata) <= self.__min_size or len(self.ydata) <= self.__min_size:
+            return self.results
+
+        self.logic()
+
+
 class NormTest(Test):
     """ Tests for whether data is normally distributed or not
     """
 
     def run(self):
         w_value, p_value = st.shapiro(self.data)
-        return w_value, p_value
+        return p_value, w_value
 
     def output(self):
         name = "Shapiro-Wilk test for normality"
@@ -59,11 +87,39 @@ class NormTest(Test):
         print name
         print "-" * len(name)
         print ""
-        print "W value = " + "{:.4f}".format(self.results[0])
-        print "p value = " + "{:.4f}".format(self.results[1])
+        print "W value = " + "{:.4f}".format(self.results[1])
+        print "p value = " + "{:.4f}".format(self.results[0])
 
     def h0(self):
         print "H0: Data is normally distributed"
 
     def ha(self):
         print "HA: Data is not normally distributed"
+
+
+class LinearRegression(Comparison):
+
+    __min_size = 3
+
+    def run(self):
+        slope, intercept, r2, p_value, std_err = st.linregress(self.xdata, self.ydata)
+        return p_value, slope, intercept, r2, std_err
+
+    def output(self):
+        name = "Linear Regression"
+        print ""
+        print name
+        print "-" * len(name)
+        print ""
+        print "slope     = " + "{:.4f}".format(self.results[1])
+        print "intercept = " + "{:.4f}".format(self.results[2])
+        print "R^2       = " + "{:.4f}".format(self.results[3])
+        print "std err   = " + "{:.4f}".format(self.results[4])
+        print "p value   = " + "{:.4f}".format(self.results[0])
+        print ""
+
+    def h0(self):
+        print "H0: There is no significant relationship between predictor and response"
+
+    def ha(self):
+        print "HA: There is a significant relationship between predictor and response"
