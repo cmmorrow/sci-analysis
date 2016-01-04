d60a46c4184192c1e29f0da4ae66c7037ead8246
cmmorrow <cmmorrow@gmail.com>
2016-01-03 20:49:14 -0600

    conf.py tweaks

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@ import sys
 import os
 import shlex
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -114,7 +116,10 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+if on_rtd:
+	html_theme = 'sphinx_rtd_theme'
+else:
+	html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
