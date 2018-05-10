c6eb120d1855cc028ba0f1b9723807b65c606fbc
Chris Morrow <cmmorrow@gmail.com>
2018-05-10 01:10:56 -0500

    README.md fix for pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as readme:
+with open('README.md', encoding='utf-8') as readme:
     long_description = readme.read()
 
 setup(
     name='sci_analysis',
-    version='2.1.1rc0',
+    version='2.1.1',
+    description='An easy to use and powerful python-based data exploration and analysis tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     url='https://github.com/cmmorrow/sci-analysis',
     license='MIT License',
@@ -30,9 +33,6 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     keywords='statistics data EDA graphing visualization analysis scientific',
-    description='An easy to use and powerful python-based data exploration and analysis tool',
-    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'six', 'm2r'],
+    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'six'],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, >=3.5',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
 )
