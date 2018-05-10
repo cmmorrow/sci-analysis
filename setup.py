817d646be32524cc08faf746a18e0f385ce3ee3a
Chris Morrow <cmmorrow@gmail.com>
2018-05-10 00:18:35 -0500

    Add m2r to requirements

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     ],
     keywords='statistics data EDA graphing visualization analysis scientific',
     description='An easy to use and powerful python-based data exploration and analysis tool',
-    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'six'],
+    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'six', 'm2r'],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, >=3.5',
     long_description=long_description,
     long_description_content_type='text/markdown',
